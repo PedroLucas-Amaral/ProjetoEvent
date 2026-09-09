e8b611babbd6b9002f47abceb5980cecba5cd22e
Pedro Lucas <117475540+PedroLucas-Amaral@users.noreply.github.com>
2026-09-09 13:42:18 -0300

    Delete espaco-eventos-api directory

--- espaco-eventos-api/src/main/java/com/example/espacoeventosapi/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.espacoeventosapi.repository;
-
-import com.example.espacoeventosapi.usuario.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
